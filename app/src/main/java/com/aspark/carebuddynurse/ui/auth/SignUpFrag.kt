68276e8bf61ef73f34345426d37741a58c331270
Ashutosh Gupta <68068357+Ashutoshgupta22@users.noreply.github.com>
2023-07-29 02:29:10 +0530

    added group chips to choose specialities during signup

--- app/src/main/java/com/aspark/carebuddynurse/ui/auth/SignUpFrag.kt
+++ app/src/main/java/com/aspark/carebuddynurse/ui/auth/SignUpFrag.kt
@@ -16,6 +16,8 @@ import androidx.navigation.fragment.findNavController
 import com.aspark.carebuddynurse.databinding.FragmentSignupBinding
 import com.aspark.carebuddynurse.model.Nurse
 import com.aspark.carebuddynurse.model.Nurse.Companion.currentNurse
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.dialog.MaterialDialogs
 import com.google.android.material.snackbar.Snackbar
@@ -29,7 +31,6 @@ class SignUpFrag: Fragment() {
 
     private lateinit var binding: FragmentSignupBinding
     private val authViewModel : AuthViewModel by activityViewModels()
-    private var selectedSpecialitiesList = arrayListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,7 +50,6 @@ class SignUpFrag: Fragment() {
             it?.let {
 
                 if (it) {
-
                     val action = SignUpFragDirections.actionSignUpFragToLoginFrag()
                     navController.navigate(action)
                     //navController.popBackStack()
@@ -58,7 +58,6 @@ class SignUpFrag: Fragment() {
         }
 
         authViewModel.signUpFailedError.observe(viewLifecycleOwner){
-
             it?.let { showSignUpFailed(it) }
         }
 
@@ -66,144 +65,28 @@ class SignUpFrag: Fragment() {
             "Elder Care","General Care","Mother & Baby Care",
             "Post-surgical Care", "Wound Care")
 
-        var checkedCount = 0
-        var isCheckedList = booleanArrayOf(false, false, false, false, false, false,
-            false, false)
+        val selectedChip = arrayListOf<Chip>()
 
+        for ( i in specialitiesList) {
 
-        binding.tvSpecialities.setOnClickListener {
+            val chip = Chip(requireContext())
+            chip.text = i
+            chip.isCheckable = true
+            binding.chipGroupSpecialities.addView(chip)
 
-             lateinit var alertDialog: AlertDialog
+            //group.checkedStateChangeListener does not work with multi selection mode
+            chip.setOnCheckedChangeListener { _, isChecked ->
 
-//            val checkboxes = arrayOfNulls<CheckBox>(8)
-//
-//            val builder = AlertDialog.Builder(requireContext())
-//            builder.setTitle("Select Options")
-//
-//            for (i in 0 until checkboxes.size) {
-//                val checkbox = CheckBox(requireContext())
-//                checkbox.id = i
-//                checkboxes[i] = checkbox
-//                builder.setPositiveButton(
-//                    "OK",
-//                    DialogInterface.OnClickListener { dialog, id ->
-//                        // Disable the rest of the checkboxes if 3 of them are checked
-//                        if (checkboxes.count { it?.isChecked ?: false } == 3) {
-//                            for (checkbox in checkboxes) {
-//                                if (checkbox?.id ?: -1 > 2) {
-//                                    checkbox?.isEnabled = false
-//                                }
-//                            }
-//                        }
-//                    }
-//                )
-//            }
-//
-//            builder.setMultiChoiceItems(
-//                arrayOf("Option 1", "Option 2", "Option 3","option 4", "option 5"),
-//                null,
-//                DialogInterface.OnMultiChoiceClickListener { dialog, which, isChecked ->
-//                    // Update the state of the checkbox
-//                    checkboxes[which]?.isChecked = isChecked
-//                }
-//            )
-//
-//            builder.show()
-
-            val dialog = AlertDialog.Builder(requireContext())
-            dialog.setTitle("Specialities")
-
-            dialog.setMultiChoiceItems(specialitiesList, isCheckedList) { _, position, isChecked ->
-                if (isChecked && checkedCount >= 3) {
-                    // If the user tries to check more checkboxes than the limit, uncheck the checkbox
-                    alertDialog.listView.setItemChecked(position, false)
-                    Toast.makeText(requireContext(), "more than 3", Toast.LENGTH_SHORT).show()
-
-
-                } else {
-                    isCheckedList[position] = isChecked
-                    if (isChecked) {
-                        checkedCount++
-                    } else {
-                        checkedCount--
+                if (isChecked) {
+                    if (selectedChip.size == 3) {
+                        val removedChip = selectedChip.removeLast()
+                        removedChip.isChecked = false
                     }
+                    selectedChip.add(chip)
                 }
+                else selectedChip.remove(chip)
             }
-
-
-
-//            dialog.setMultiChoiceItems(specialitiesList, isCheckedList) {
-//                    _, which, isChecked ->
-//
-//                when(isChecked) {
-//
-//                    true -> {
-//                        checkedCount++
-//                        selectedSpecialitiesList.add(specialitiesList[which])
-//                        isCheckedList[which] = true
-//
-//                        if (checkedCount <=3 ){
-//
-//                            dialog.setPositiveButton("Done"){
-//                                    dialog, which ->
-//
-//                                checkedCount = 0
-//
-//                                isCheckedList = booleanArrayOf(false, false, false, false, false, false,
-//                                    false, false)
-//
-////                Log.i("SignupFrag", "Done - ${selectedSpecialitiesList[0]} " +
-////                        "${selectedSpecialitiesList[1]} ${selectedSpecialitiesList[2]}")
-//
-//                                // make sure that only 3 chosen values are checked and others are unchecked
-//                                for(i in specialitiesList.withIndex()) {
-//
-//                                    if (i.value == selectedSpecialitiesList[0] ||
-//                                        i.value == selectedSpecialitiesList[1] ||
-//                                        i.value == selectedSpecialitiesList[2]
-//                                    ) {
-//                                        checkedCount++
-//                                        isCheckedList[i.index] = true
-//                                    }
-//                                }
-//
-//
-//                            }
-//
-//                        }
-//
-//                          if(checkedCount > 3) {
-//
-//                            Log.i("SignUpFrag", "onViewCreated: 3 box checked")
-//                            Snackbar.make(binding.tvSpecialities,
-//                                "Max 3 specialities only",Snackbar.LENGTH_SHORT).show()
-//                        }
-//                    }
-//
-//                    false -> {
-//                        checkedCount --
-//
-//                            isCheckedList[which] = false
-//                            selectedSpecialitiesList.remove(specialitiesList[which])
-//                    }
-//                }
-//
-//            }
-
-            dialog.setNegativeButton("Cancel") {
-                dialog,which ->
-
-                checkedCount = 0
-                 isCheckedList = booleanArrayOf(false, false, false, false, false, false,
-                    false, false)
-                selectedSpecialitiesList = arrayListOf()
-            }
-
-            alertDialog = dialog.create()
-            alertDialog.show()
-
         }
-
 
         binding.btnSignup.setOnClickListener {
 
@@ -217,18 +100,24 @@ class SignUpFrag: Fragment() {
             val sPassword = binding.etSignupPassword.text.toString().trim()
             val sConfirmPassword = binding.etSignupConfirmPassword.text.toString().trim()
 
+            if ( selectedChip.size == 0)
+                Toast.makeText(requireContext(), "Please select a speciality",
+                    Toast.LENGTH_SHORT).show()
+
             if (verifyInput(sFirstName, sAge,sBiography, sQualification,
                     sExperience, sEmail, sPassword, sConfirmPassword)) {
 
                 val nurse = currentNurse
+
+                for ( i in selectedChip)
+                    nurse.specialities.add(i.text.toString())
+
                 nurse.firstName = sFirstName
                 nurse.lastName = sLastName
                 nurse.age = sAge.toInt()
                 nurse.biography = sBiography
                 nurse.experience = sExperience.toInt()
                 nurse.qualifications = sQualification
-                nurse.specialities = arrayListOf(selectedSpecialitiesList[0],
-                    selectedSpecialitiesList[1], selectedSpecialitiesList[2])
                 nurse.email = sEmail
                 nurse.password = sPassword
 
